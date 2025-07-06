f328eaede84ecfe2b4607815f50f66fd2e365071
moisesantunes <tec.m.antunes@gmail.com>
2025-07-06 00:04:16 -0300

    segundo, vou refazer ta ruim

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,30 @@
 async function fetchGet(url){
-   
     const response = await fetch(url)
     const data = await response.json()
-   // console.log("os trampos sao: ",data)
     return data
-    /*
-    .then((response)=>{
-        return response.json()
-    })
-    .then((data)=>{
-        console.log(data)
-        return data;
-    })
-    */
-   
 }
-
-
 
 
 function cardBasic(tarefa){
   // document.getElementById(id).innerHTML+=
-  let card = String.raw`<div class="${tarefa.estado}">
+  let card =`<div onclick="cardDetail('${tarefa.id}')" class="${tarefa.estado}">
         <h3>${tarefa.tarefa} </h3>
-        <a href="javascript:void(0)" onclick="alert('${tarefa.id}')">${tarefa.id}</a>
+        <p class="">${tarefa.descrição}</p>
     </div>`
     return card;
 }
 function cardDetail(id){
     fetchGet("/api/tarefa/"+id)
     .then((res)=>{
-        alert(res.tarefa)
+        alert(res.id)
+        let card =`<div onclick="cardDetail('${res.id}')" class="${res.estado}">
+            <h3>${res.tarefa} </h3>
+            <p class="">${res.descrição}</p>
+            <p class="">${res.criadaem}</p>
+        </div>`
+        const parser =new DOMParser().parseFromString(card,"text/html")
+    document.getElementById("fichario").innerHTML = parser.body.innerHTML
+        
     })
     return false;
 }
